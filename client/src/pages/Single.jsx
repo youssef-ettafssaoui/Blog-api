43d014083b78bf05fb25d9e08efa9dc8e8ace4fc
youssef-ettafssaoui <yettafssaoui2@gmail.com>
2022-10-02 01:22:10 +0100

    The client side design

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,7 +1,74 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
+import Delete from "../img/delete.png";
+import Edit from "../img/edit.png";
 
 const Single = () => {
-  return <div></div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <div className="info">
+            <span>Youssef</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
+          excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
+          ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
+          corporis!
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
+            excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
+            ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
+            corporis!
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
+            excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
+            ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
+            corporis!
+          </p>
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
+          excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
+          ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
+          corporis!
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
+            excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
+            ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
+            corporis!
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
+            excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
+            ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
+            corporis!
+          </p>
+        </p>
+      </div>
+      <Menu />
+    </div>
+  );
 };
 
 export default Single;
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Write = () => {
-  return <div></div>;
+  const [value, setValue] = useState("");
+  return (
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill theme="snow" value={value} onChange={setValue} />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status : </b> Draft
+          </span>
+          <span>
+            <b>Visibility : </b> Public
+          </span>
+          <input style={{ display: "none" }} type="file" name="" id="file" />
+          <label htmlFor="file">Upload Image</label>
+          <div className="buttons">
+            <button>Save as DRAFT</button>
+            <button>Update</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Category</h1>
+          <div className="cat">
+            <input type="radio" name="cat" value="front" id="front" />
+            <label htmlFor="front">Front</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="back" id="back" />
+            <label htmlFor="back">Back</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="Networks" id="mobile" />
+            <label htmlFor="mobile">Mobile</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="IA" id="IA" />
+            <label htmlFor="IA">IA</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="Databases" id="Databases" />
+            <label htmlFor="Databases">Databases</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="Networks" id="Networks" />
+            <label htmlFor="Networks">Networks</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Write;
